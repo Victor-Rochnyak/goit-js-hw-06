5a72642f70bcf0c45f54709b1627f6cc9281da7d
Victor-Rochnyak <victor.rochnyak@gmail.com>
2022-08-23 16:58:21 +0300

    Переробив ДЗ-2

--- js/task-02.js
+++ js/task-02.js
@@ -7,36 +7,66 @@ const ingredients = [
   'Condiments',
 ];
 
-const itemEl = document.createElement('li');
-itemEl.classList.add('item');
-itemEl.textContent = 'Potatoes';
-console.log(itemEl);
 
-const itemEl1 = document.createElement('li');
-itemEl1.classList.add('item');
-itemEl1.textContent = 'Mushrooms';
-console.log(itemEl1);
+const inrgedientsElement = document.querySelector('#ingredients');
+const itemElement = ingredients.map(ingredients => {
+  const itemEl= document.createElement('li');
+  itemEl.textContent = `${ingredients}`;
+  itemEl.classList.add('item');
+    return itemEl;
+});
+inrgedientsElement.append(...itemElement);
+console.log(inrgedientsElement)
 
-const itemEl2 = document.createElement('li');
-itemEl2.classList.add('item');
-itemEl2.textContent = 'Garlic';
-console.log(itemEl2);
+// 
 
-const itemEl3 = document.createElement('li');
-itemEl3.classList.add('item');
-itemEl3.textContent = 'Tomatos';
-console.log(itemEl3);
+// const ingredientsEl = document.querySelector('#ingredients');
+// const itemEl = ingredients.map(ingredient => {
+//   const ingredientItem = document.createElement('li');
+//     ingredientItem.textContent = `${ingredient}`;
+//     ingredientItem.classList.add('item');
+//     return ingredientItem;
+// });
+// ingredientsEl.append(...itemEl);
 
-const itemEl4 = document.createElement('li');
-itemEl4.classList.add('item');
-itemEl4.textContent = 'Herbs';
-console.log(itemEl4);
+// .join("");
+// Напиши скрипт, который для каждого элемента массива ingredients:
 
-const itemEl5 = document.createElement('li');
-itemEl5.classList.add('item');
-itemEl5.textContent = 'Condiments';
-console.log(itemEl5);
+// Создаст отдельный элемент <li>. Обзательно используй метод document.createElement().
+// Добавит название ингредиента как его текстовое содержимое.
+// Добавит элементу класс item.
+// После чего вставит все <li> за одну операцию в список ul#ingredients.
 
-const ingredientsEl = document.querySelector('#ingredients');
-// ingredientsEl.appendChild(itemEl);
-ingredientsEl.append(itemEl, itemEl1, itemEl2, itemEl3, itemEl4, itemEl5);
+// const itemEl = document.createElement('li');
+// itemEl.classList.add('item');
+// itemEl.textContent = 'Potatoes';
+// console.log(itemEl);
+
+// const itemEl1 = document.createElement('li');
+// itemEl1.classList.add('item');
+// itemEl1.textContent = 'Mushrooms';
+// console.log(itemEl1);
+
+// const itemEl2 = document.createElement('li');
+// itemEl2.classList.add('item');
+// itemEl2.textContent = 'Garlic';
+// console.log(itemEl2);
+
+// const itemEl3 = document.createElement('li');
+// itemEl3.classList.add('item');
+// itemEl3.textContent = 'Tomatos';
+// console.log(itemEl3);
+
+// const itemEl4 = document.createElement('li');
+// itemEl4.classList.add('item');
+// itemEl4.textContent = 'Herbs';
+// console.log(itemEl4);
+
+// const itemEl5 = document.createElement('li');
+// itemEl5.classList.add('item');
+// itemEl5.textContent = 'Condiments';
+// console.log(itemEl5);
+
+// const ingredientsEl = document.querySelector('#ingredients');
+// // ingredientsEl.appendChild(itemEl);
+// ingredientsEl.append(itemEl, itemEl1, itemEl2, itemEl3, itemEl4, itemEl5);
